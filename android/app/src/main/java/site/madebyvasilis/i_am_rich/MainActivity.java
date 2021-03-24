d7a6ac322d42d6f15324f4ffbad93d6c882087e7
Vasilis Skarleas <vskarleas@icloud.com>
2021-03-24 11:23:42 +0200

    Initial commit

---
+++ android/app/src/main/java/site/madebyvasilis/i_am_rich/MainActivity.java
@@ -0,0 +1,6 @@
+package site.madebyvasilis.i_am_rich;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+//The main function is the starting point for all our Flutter apps.
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I am reach'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://flutter-app.s3.us-west-000.backblazeb2.com/unnamed.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
